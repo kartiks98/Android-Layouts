d664e832e57ea2b25dadacccbb00dd6dd2d7f15e
Kartik Saini <kartiksaini135@gmail.com>
2019-02-20 19:38:18 +0530

    i don't know what kartik has done

--- app/src/main/java/saini/kartik/MainActivity.java
+++ app/src/main/java/saini/kartik/MainActivity.java
@@ -2,12 +2,33 @@ package saini.kartik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
     }
 }
